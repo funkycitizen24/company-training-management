02ed0984a51fb808a648f838e203a443424f8bec
funkycitizen24 <mirunalupas21@gmail.com>
2023-08-26 20:43:02 +0300

    Initial commit

---
+++ src/main/java/com/nttdata/companytrainingmanagement/CompanyTrainingManagementApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.companytrainingmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyTrainingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyTrainingManagementApplication.class, args);
+	}
+
+}
